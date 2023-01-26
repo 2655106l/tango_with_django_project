3a62e92237b381fdfd5b640edae36ce992abb751
“Nathan <2655106l@student.gla.ac.uk>
2023-01-26 21:31:42 +0000

    Completed ch 5

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -2,6 +2,8 @@ from rango import views
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 """tango_with_django_project URL Configuration
@@ -25,4 +27,4 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('rango/', include('rango.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
